b6d48fb41a1454da57108629a2f5c8c789b557ef
MurilloAvellaneda <murillo.avellaneda@gmail.com>
2021-12-07 21:42:49 -0300

    adicionando a estrutura do array de objetos para as cartas e container para imagens

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+// Player array cards
+const playerEnterpriseCards = [
+    {
+        name: "A",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageA',
+    },
+    {
+        name: "B",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageB',
+    },
+    {
+        name: "C",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageC',
+    }
+]  
+        
+
+// Oponent array cards
+
+const oponentEnterpriseCards = [
+    {
+        name: "D",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageD',
+    },
+    {
+        name: "E",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageE',
+    },
+    {
+        name: "F",
+        priceOverProfits: 0,
+        dividendYeld: 0,
+        actualPrice: 0,
+        image: 'imageF',
+    }
+] 
