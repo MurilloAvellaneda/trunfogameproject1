4310441381c4796f9591bda0aed445cd5ecaafab
MurilloAvellaneda <murillo.avellaneda@gmail.com>
2021-12-09 21:41:12 -0300

    definindo as propriedas do array de cartas e adicionando funcionalidade ao botão de sortear empresa

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,50 @@
-// Player array cards
-const playerEnterpriseCards = [
+// Cards array
+const enterpriseCards = [
     {
-        name: "A",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageA',
+        ticker: "VALE3",
+        priceOverProfits: 4.03,
+        dividendYield: 19.48,
+        roe: 52.78,
+        image: "../images/vale-logo.png",
     },
     {
-        name: "B",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageB',
+        ticker: "WEGE3",
+        priceOverProfits: 44.35,
+        dividendYield: 1.12,
+        roe: 27.09,
+        image: "../images/weg-logo.png",
     },
     {
-        name: "C",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageC',
+        ticker: "TAEE11",
+        priceOverProfits: 4.67,
+        dividendYield: 13.13,
+        roe: 37.82,
+        image: '../images/taesa-logo.png',
+    },
+    {
+        ticker: "GGBR3",
+        priceOverProfits: 3.08,
+        dividendYield: 13.91,
+        roe: 31.63,
+        image: '../images/gerdau-logo.png',
+    },
+    {
+        ticker: "MGLU3",
+        priceOverProfits: 58.44,
+        dividendYield: 0.67,
+        roe: 6.6,
+        image: '../images/magalu-logo.png',
     }
-]  
-        
+]
 
-// Oponent array cards
+const sortPlayerCard = (array) => {
+    return array[Math.floor(Math.random()*array.length)]
+}
 
-const oponentEnterpriseCards = [
-    {
-        name: "D",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageD',
-    },
-    {
-        name: "E",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageE',
-    },
-    {
-        name: "F",
-        priceOverProfits: 0,
-        dividendYeld: 0,
-        actualPrice: 0,
-        image: 'imageF',
-    }
-] 
+document.getElementById("player-sort-btn").addEventListener("click", () => {
+    const stock = sortPlayerCard(enterpriseCards)
+    document.getElementById("logo-stock-player").src=stock.image;
+    document.getElementById("price-over-profits-label").innerHTML = `P/L: ${stock.priceOverProfits}`;
+    document.getElementById("dividend-yield-label").innerHTML = `DY: ${stock.dividendYield}%`;
+    document.getElementById("roe-label").innerHTML = `ROE: ${stock.roe}%`;
+});
