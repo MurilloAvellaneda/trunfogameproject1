1c52620ab07cdb03aff3be14a42674940cf75abf
MurilloAvellaneda <murillo.avellaneda@gmail.com>
2021-12-12 09:33:08 -0300

    change path to images

--- scripts/script.js
+++ scripts/script.js
@@ -5,35 +5,35 @@ const enterpriseCards = [
         priceOverProfits: 4.03,
         dividendYield: 19.48,
         roe: 52.78,
-        image: "../images/vale-logo.png",
+        image: "./images/vale-logo.png",
     },
     {
         ticker: "WEGE3",
         priceOverProfits: 44.35,
         dividendYield: 1.12,
         roe: 27.09,
-        image: "../images/weg-logo.png",
+        image: "./images/weg-logo.png",
     },
     {
         ticker: "TAEE11",
         priceOverProfits: 4.67,
         dividendYield: 13.13,
         roe: 37.82,
-        image: '../images/taesa-logo.png',
+        image: './images/taesa-logo.png',
     },
     {
         ticker: "GGBR3",
         priceOverProfits: 3.08,
         dividendYield: 13.91,
         roe: 31.63,
-        image: '../images/gerdau-logo.png',
+        image: './images/gerdau-logo.png',
     },
     {
         ticker: "MGLU3",
         priceOverProfits: 58.44,
         dividendYield: 0.67,
         roe: 6.6,
-        image: '../images/magalu-logo.png',
+        image: './images/magalu-logo.png',
     }
 ]
 
